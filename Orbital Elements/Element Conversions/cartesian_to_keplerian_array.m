582d9ab4a0b3d90bbcaad56e711eafaf867e0b4b
thatfrtd <thatfrtd@gmail.com>
2026-09-19 18:44:13 -0400

    Almost fixed porkchop

--- Orbital Elements/Element Conversions/cartesian_to_keplerian_array.m
+++ Orbital Elements/Element Conversions/cartesian_to_keplerian_array.m
@@ -1,4 +1,10 @@
-function [x_keplerian, nu] = cartesian_to_keplerian_array(x_cartesian_array, Khat, Ihat, mu)
+function [x_keplerian, nu] = cartesian_to_keplerian_array(x_cartesian_array, mu, Khat, Ihat)
+arguments
+    x_cartesian_array
+    mu
+    Khat = [0; 0; 1] % z direction
+    Ihat = [1; 0; 0] % x direction
+end
 %CARTESIAN_TO_KEPLERIAN_ARRAY Summary of this function goes here
 %   Detailed explanation goes here
     x_keplerian = zeros(6, size(x_cartesian_array, 2));
@@ -7,6 +13,6 @@ function [x_keplerian, nu] = cartesian_to_keplerian_array(x_cartesian_array, Kha
     for ind = 1:size(x_cartesian_array, 2)
         x_cartesian = x_cartesian_array(:, ind);
 
-        [x_keplerian(:, ind), nu(ind)] = cartesian_to_keplerian(x_cartesian, Khat, Ihat, mu);
+        [x_keplerian(:, ind), nu(ind)] = cartesian_to_keplerian(x_cartesian, mu, Khat, Ihat);
     end
 end
